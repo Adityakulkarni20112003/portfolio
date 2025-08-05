5fcbf5739bb129eeb60686c8637bd5b081b692ed
adityakulkarni2003 <adityadkulkarni2003@gmail.com>
2025-08-05 14:55:34 +0530

    adityak3

--- server/index.ts
+++ server/index.ts
@@ -62,6 +62,6 @@ app.use((req, res, next) => {
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
   server.listen(port, () => {
-    log(`serving on port ${port}`);
+    console.log(`serving on port ${port}`);
   });
 })();
